de0e41c8d45db82d62dc6ef16bdb956fe07c9804
Jaycee Han <jaycee@macpractice.com>
2017-04-19 14:06:05 -0500

    test0419

--- SpeakLine/SpeakLine/AppDelegate.h
+++ SpeakLine/SpeakLine/AppDelegate.h
@@ -8,8 +8,19 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSSpeechSynthesizerDelegate>
+{
+	NSSpeechSynthesizer *_speechSynth;
+}
+@property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *textField;
 
+
+@property (weak) IBOutlet NSButton *stopButton;
+@property (weak) IBOutlet NSButton *speakButton;
+
+- (IBAction)stopit:(id)sender;
+- (IBAction)sayit:(id)sender;
 
 @end
 
